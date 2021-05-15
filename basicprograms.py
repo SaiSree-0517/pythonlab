960032bf736138f85989294436ade2b7cf93be28
SaiSree-0517 <69571289+SaiSree-0517@users.noreply.github.com>
2021-05-15 10:19:37 +0530

    Update basicprograms.py

--- basicprograms.py
+++ basicprograms.py
@@ -1,4 +1,4 @@
-#1.1) Running instructions in Interactive interpreter and a Python Scrip
+#1.1) Running instructions in Interactive interpreter and a Python Script
 #Interactive interpreter
 >>> a=10
 >>> print(a)
@@ -27,3 +27,29 @@ print('reversed number=',rev)
 #output
 enter the number123456
 reversed number= 654321
+#1.2) Implement a python script to purposefully raise Indentation Error and Correct it
+#with indentation error
+i=1
+while i<=10:
+print(i)#indentation error
+    i+=1
+print('i=',i)
+#after correcting indentation error
+i=1
+while i<=10:
+    print(i)
+    i+=1
+print('i=',i)
+#output:
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+i= 11
+>>> 
