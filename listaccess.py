45f98547a7a49f25f9ad10dbd523617525f07304
SaiSree-0517 <69571289+SaiSree-0517@users.noreply.github.com>
2021-07-10 14:55:53 +0530

    Add files via upload

---
+++ listaccess.py
@@ -0,0 +1,4 @@
+from listoperations import *
+print(list_sum([1,2,3,4,5]))
+print(list_sort([9,2,3,4,10]))
+print(string_list_conversions("hello world"))
---
+++ listoperations.py
@@ -0,0 +1,11 @@
+def list_sum(l):
+    s=0
+    for i in l:
+        s+=i
+    return s
+def list_sort(l):
+    l.sort()
+    return l
+def string_list_conversions(s):
+    li = list(s)
+    return li
