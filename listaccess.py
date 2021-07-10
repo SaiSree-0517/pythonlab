89f283ddbaa633f09c052f29a442da3b77874012
SaiSree-0517 <69571289+SaiSree-0517@users.noreply.github.com>
2021-07-10 14:56:18 +0530

    Update listaccess.py

--- listaccess.py
+++ listaccess.py
@@ -2,3 +2,8 @@ from listoperations import *
 print(list_sum([1,2,3,4,5]))
 print(list_sort([9,2,3,4,10]))
 print(string_list_conversions("hello world"))
+"""
+15
+[2, 3, 4, 9, 10]
+['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd']
+"""
